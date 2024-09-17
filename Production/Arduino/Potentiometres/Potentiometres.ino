015e77b15f81314663bb5d5f73ff4c6a600fbfed
Sasha Bédard <85566308+sashabedard@users.noreply.github.com>
2024-09-17 15:08:37 -0400

    updated potentiometres

--- Production/Arduino/Potentiometres/Potentiometres.ino
+++ Production/Arduino/Potentiometres/Potentiometres.ino
@@ -5,22 +5,30 @@ void setup() {
 
 void loop() {
   int sensorValue = analogRead(A0);
-
+  //Serial.println(sensorValue); //DEBUG
   delay(250);  // delay in between reads for stability
 
-  if(sensorValue && sensorValue != 0 && sensorValue <= 250){
-    Serial.println("monde 1");
+  if(sensorValue == 0){
+    sensorValue = 1;
+  }
+  
+  if(sensorValue && sensorValue >= 0 && sensorValue <= 204){
+    Serial.println("gi1");
   }
 
-  else if(sensorValue && sensorValue != 0 && sensorValue > 251 && sensorValue <= 500){
-    Serial.println("monde 2");
+  else if(sensorValue && sensorValue != 0 && sensorValue > 205 && sensorValue <= 409){
+    Serial.println("2");
   }
 
-  else if(sensorValue && sensorValue != 0 && sensorValue > 501 && sensorValue <= 750){
-    Serial.println("monde 3");
+  else if(sensorValue && sensorValue != 0 && sensorValue > 410 && sensorValue <= 613){
+    Serial.println("3");
   }
 
-  else if(sensorValue && sensorValue != 0 && sensorValue > 751){
-    Serial.println("monde 4");
+  else if(sensorValue && sensorValue != 0 && sensorValue > 614 && sensorValue <=818){
+    Serial.println("4");
+  }
+
+    else if(sensorValue && sensorValue != 0 && sensorValue > 819){
+    Serial.println("5");
   }
 }
